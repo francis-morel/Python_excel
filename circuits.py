aa68c1968deb08395f9ad69c04e5e5e07fb32455
Francis <poste18@MJL.local>
2021-06-03 12:25:04 -0400

    Modification pour toujours prendre la colonne la plus longue au lieux de la dernière

---
+++ circuits.py
@@ -0,0 +1,10 @@
+fusible = 1
+
+
+for i in range(1, 20):
+    for j in range(1, 5):
+        print("FU" + str(fusible) + " L" + str(i) + "-" + str(j))
+
+        fusible += 1
+
+input("Press enter to stop")
